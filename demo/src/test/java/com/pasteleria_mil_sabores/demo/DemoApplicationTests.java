dd1fc90f49f281577f3abcaf699240416029ebda
yaquelin2305 <ya.rugel@duocuc.cl>
2025-11-10 17:31:28 -0300

    Proyecto Pastelería inicial con Spring Boot y MySQL

---
+++ demo/src/test/java/com/pasteleria_mil_sabores/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pasteleria_mil_sabores.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
